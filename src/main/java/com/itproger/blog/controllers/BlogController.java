19bec6b7acd4de29e1335bc29e52db868bb12708
unknown <mus-t-ang777@mail.ru>
2025-11-07 21:14:39 +0400

    update ver2

---
+++ src/main/java/com/itproger/blog/controllers/BlogController.java
@@ -0,0 +1,14 @@
+package com.itproger.blog.controllers;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        return "blog-main";
+    }
+}
