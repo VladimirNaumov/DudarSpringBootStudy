4784f5b46f550a52b6c8a28abec53b3a8731c3c7
Vladimir Naumov <mus-t-ang777@mail.ru>
2025-11-10 16:47:55 +0400

    add maket table+postgres

--- src/main/java/com/itproger/blog/controllers/BlogController.java
+++ src/main/java/com/itproger/blog/controllers/BlogController.java
@@ -1,14 +1,22 @@
 package com.itproger.blog.controllers;
 
-import ch.qos.logback.core.model.Model;
+import com.itproger.blog.models.Post;
+import com.itproger.blog.repo.postRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class BlogController {
 
+    @Autowired
+    private postRepository postRepository;  //чтобы передавать все записи в шаблон ( указываем репозиторий и переменную)
+
     @GetMapping("/blog")
     public String blogMain(Model model){
+        Iterable<Post> posts = postRepository.findAll();   //массив данных, в котором все записи из таблицы о каком то обьекте
+        model.addAttribute("posts",posts);     //обращение к модели, и передача верхнего массива  с именем пост
         return "blog-main";
     }
 }
